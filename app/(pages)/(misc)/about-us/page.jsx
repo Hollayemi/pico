79ffb0ce1269b02efa0cc8fc0fdc2ac44f331376
Akinwalire Olanrewaju <olanrewajuakinwalire@gmail.com>
2025-07-23 20:53:37 +0100

    Side First commit

---
+++ app/(pages)/(misc)/about-us/page.jsx
@@ -0,0 +1,10 @@
+import HomeWrapper from "@/app/components/wrapper";
+import React from "react";
+
+const page = () => {
+  return <HomeWrapper>
+    
+  </HomeWrapper>;
+};
+
+export default page;
--- app/components/wrapper/index.jsx
+++ app/components/wrapper/index.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import Header from './header'
+import React from "react";
+import Header from "./header";
 
 export const HomeWrapper = ({ children }) => {
-    return (
-        <div>
-            <Header />
-            {children}
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
+};
 
-export default HomeWrapper
+export default HomeWrapper;
